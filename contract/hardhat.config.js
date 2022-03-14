8adcb002de98a3bc78313cff756e2a461e878dbc
captain-woof <captainwoof.official@gmail.com>
2022-03-14 20:32:14 +0530

    Added configs for Mumbai polygon testnet

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
