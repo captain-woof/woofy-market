ff0e8f8861a5a5b654da6452530763bdd46a6b13
captain-woof <captainwoof.official@gmail.com>
2022-03-16 21:48:31 +0530

    Deploy script updated

--- contract/scripts/deploy_nft_contract.js
+++ contract/scripts/deploy_nft_contract.js
@@ -1,8 +1,16 @@
+const { ethers } = require("hardhat");
 
 // Deploys the smart contract
 (async () => {
     const contract = await hre.ethers.getContractFactory("Woofy");
-    const deployedContract = await contract.deploy();
+    const deployedContract = await contract.deploy(
+        ethers.utils.parseEther("0.1"),
+        15 * 60,
+        50,
+        {
+            value: ethers.utils.parseEther("2")
+        }
+    );
     await deployedContract.deployed();
     console.log(`Woofy contract deployed to: ${deployedContract.address}`);
 })()
